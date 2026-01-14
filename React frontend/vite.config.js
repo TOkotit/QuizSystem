e7e457f516e46db5ad4c6e0cc0514f9fa4f4fb6b
zyuzyakin <zyuzyakinIvan06@gmail.com>
2026-01-14 10:18:58 +0500

    fix npm

--- React frontend/vite.config.js
+++ React frontend/vite.config.js
@@ -3,10 +3,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    dts({ 
+      insertTypesEntry: true, // Создает файл index.d.ts
+      include: ['src'],       // Откуда брать файлы
+    })
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.js'),
@@ -14,15 +20,20 @@ export default defineConfig({
       fileName: (format) => `poll-test-widgets.${format}.js`
     },
     rollupOptions: {
-      // Исключаем React из сборки, чтобы не было дубликатов
-      external: ['react', 'react-dom', '@xyflow/react'], 
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-          '@xyflow/react': 'ReactFlow'
-        }
-      }
+  external: [
+    'react', 
+    'react-dom', 
+    'react/jsx-runtime', 
+    '@xyflow/react'
+  ],
+  output: {
+    globals: {
+      react: 'React',
+      'react-dom': 'ReactDOM',
+      'react/jsx-runtime': 'jsxRuntime', 
+      '@xyflow/react': 'ReactFlow'
     }
+  }
+}
   }
 })
